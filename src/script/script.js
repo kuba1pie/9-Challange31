319f40431a1089cb387b54411d6d3e2bf1b327d1
kuba1pie <kuba1pie@gmail.com>
2019-10-30 15:49:51 +0100

    scrool effects

--- src/script/script.js
+++ src/script/script.js
@@ -1,16 +1,10 @@
- $( document ).ready(function() {
-  //adding brake mark after h3
+$( document ).ready(function() {
+  scrollTo();
   $( "main h3" ).after('<div class="broken"></div>');
-  //parse skills
-  $.getJSON("src/skills.json", function(data){
-    for(let i = 0; i < data.length; i++) {
-      let obj = data[i];
-      let name=obj.name;
-      let progress=obj.progress;
-      let el = $('<div>').attr('class','item').append("<p>" + name + "</p><div class='myProgress'><div class='myBar' style='width:"+ progress +"%'></div></div>") 
-      $("#cont_skills").append(el);
-    };})
-  //parse portfolio
+  skills();
+  portfolio();
+});
+function portfolio(){
   $.getJSON("src/portfolio.json", function(data){
     for(let i = 0; i < data.length; i++) {
       let obj = data[i];
@@ -23,15 +17,23 @@
         "<div id='project"+id+"class='shadowbox'><p>" + name + "</p></div>") 
       $("#cont_works").append(el);
     };})
-});
-//show more items in portfolio
+}
 function show(){
-  console.log("dupoa")
   for (let i=4; i<7; i++) {
     console.log("#project"+i)
     $("#project"+i).slideDown();
   }
   $("#showButton").css("display","none")
+}
+function skills(){
+  $.getJSON("src/skills.json", function(data){
+    for(let i = 0; i < data.length; i++) {
+      let obj = data[i];
+      let name=obj.name;
+      let progress=obj.progress;
+      let el = $('<div>').attr('class','item').append("<p>" + name + "</p><div class='myProgress'><div class='myBar' style='width:"+ progress +"%'></div></div>") 
+      $("#cont_skills").append(el);
+    };})
 }
 //ticks datas and limits
   let tick1s = setInterval(tick1, 1000);
@@ -61,5 +63,27 @@ function tick01(){
 function topjump(){
   $('html, body').animate({ scrollTop: 0 }, 'fast');
 }
-
-
+function scrollTo() {
+// Vanilla JavaScript Scroll to Anchor
+// @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
+	const links = document.querySelectorAll('.myButton');
+	links.forEach(each => (each.onclick = scrollAnchors));
+}
+function scrollAnchors(e, respond = null) {
+	const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+	e.preventDefault();
+	let targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+	const targetAnchor = document.querySelector(targetID);
+	if (!targetAnchor) return;
+	const originalTop = distanceToTop(targetAnchor);
+	window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+	const checkIfDone = setInterval(function() {
+		const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+		if (distanceToTop(targetAnchor) === 0 || atBottom) {
+			targetAnchor.tabIndex = '-1';
+			targetAnchor.focus();
+			window.history.pushState('', '', targetID);
+			clearInterval(checkIfDone);
+		}
+	}, 100);
+}
